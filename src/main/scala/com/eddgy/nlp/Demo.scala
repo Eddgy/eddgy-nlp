fb3e4e22fc0df5717901dd107548c8e0c413e4a9
Adam Pingel <pingel@gmail.com>
2012-10-21 21:07:39 -0700

    updating for new kmeans api

--- src/main/scala/com/eddgy/nlp/Demo.scala
+++ src/main/scala/com/eddgy/nlp/Demo.scala
@@ -9,9 +9,9 @@ object Demo {
 
   def withoutAxle(): Unit = {
     import com.eddgy.nlp.transformer.PcaTransformer
-    val df = EuclideanDistance // or CosineDistance or ManhattanDistance
-    val points = articles.map(article => Point(fullFeatures(article))) // or simpleFeatures
-    val pointTransformer = new PcaTransformer(points, 0.95) // or IdentityTransformer or ZscoreTransformer(points)
+    val df = EuclideanDistance
+    val points = articles.map(article => Point(fullFeatures(article)))
+    val pointTransformer = new PcaTransformer(points, 0.95)
     val transformedPoints = pointTransformer(points)
     val km = new Kmeans(transformedPoints, df, 0.0001, numIterations)
     val (x, centroids) = km.run(k) // second argument is 'restarts'?
@@ -22,18 +22,18 @@ object Demo {
   def withAxle(): Unit = {
 
     import axle.matrix.JblasMatrixFactory._
-    import axle.ml.KMeans._
+    import axle.ml.KMeans
     import axle.ml.DistanceFunction._
 
     // TODO: use 0.0001
     // TODO: numFeatures should be inferred
 
-    val distance = ManhattanDistanceFunction
+    val distance = EuclideanDistanceFunction
     val featureExtractor = (article: FederalistArticle) => fullFeatures(article)
     val labelExtractor = (article: FederalistArticle) => article.author
     val constructor = (features: Seq[Double]) => null.asInstanceOf[FederalistArticle] // TODO
 
-    val classifier = cluster(articles, numFullFeatures, featureExtractor, constructor, distance, k, numIterations)
+    val classifier = KMeans(articles, numFullFeatures, featureExtractor, constructor, distance, k, numIterations)
     val confusionMatrix = classifier.confusionMatrix(articles, labelExtractor)
   }
 
